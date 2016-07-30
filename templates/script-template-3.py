b310aa4ed1fefd542dd956cd4e1bd26344dd2bc6
Tom Elliott <tom.elliott@nyu.edu>
2016-07-30 18:42:10 -0500

    improve handling of boolean arguments and logging

--- templates/script-template-3.py
+++ templates/script-template-3.py
@@ -11,7 +11,7 @@ import re
 import sys
 import traceback
 
-DEFAULT_LOG_LEVEL = logging.INFO
+DEFAULT_LOG_LEVEL = logging.WARNING
 POSITIONAL_ARGUMENTS = [
     ['-l', '--loglevel', logging.getLevelName(DEFAULT_LOG_LEVEL),
         'desired logging level (' +
@@ -47,32 +47,34 @@ if __name__ == "__main__":
     log_level = DEFAULT_LOG_LEVEL
     log_level_name = logging.getLevelName(log_level)
     logging.basicConfig(level=log_level)
-
-    # report script name and docstring
-    fn_this = inspect.stack()[0][1].strip()
-    title_this = __doc__.strip()
-    logging.info(': '.join((fn_this, title_this)))
     try:
         parser = argparse.ArgumentParser(
             description=__doc__,
             formatter_class=argparse.ArgumentDefaultsHelpFormatter)
         for p in POSITIONAL_ARGUMENTS:
             d = {
-                'default': p[2],
                 'help': p[3]
             }
             if type(p[2]) == bool:
-                d['action'] = 'store_true'
+                if p[2] is False:
+                    d['action'] = 'store_true'
+                    d['default'] = False
+                else:
+                    d['action'] = 'store_false'
+                    d['default'] = True
+            else:
+                d['default'] = p[2]
             parser.add_argument(
                 p[0],
                 p[1],
-                d)
-        parser.add_argument(
-            'where',
-            metavar='N',
-            type=str,
-            nargs='1',
-            help='path to desired project directory')
+                **d)
+        # example positional argument
+        # parser.add_argument(
+        #     'foo',
+        #     metavar='N',
+        #     type=str,
+        #     nargs='1',
+        #     help="foo is better than bar except when it isn't")
         args = parser.parse_args()
         if args.loglevel is not None:
             args_log_level = re.sub('\s+', '', args.loglevel.strip().upper())
@@ -89,6 +91,9 @@ if __name__ == "__main__":
             log_level = logging.INFO
         log_level_name = logging.getLevelName(log_level)
         logging.getLogger().setLevel(log_level)
+        fn_this = inspect.stack()[0][1].strip()
+        title_this = __doc__.strip()
+        logging.info(': '.join((fn_this, title_this)))
         if log_level != DEFAULT_LOG_LEVEL:
             logging.warning(
                 "logging level changed to %s via command line option"
