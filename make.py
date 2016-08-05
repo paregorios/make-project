4d9033195461a99c2923ba51059e44550b2ca884
Tom Elliott <tom.elliott@nyu.edu>
2016-08-05 15:20:29 -0500

    add path to README template

--- make.py
+++ make.py
@@ -42,7 +42,8 @@ template_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)),
                             'templates')
 TEMPLATES = {
     'script-2': os.path.join(template_dir, 'script-template-2.py'),
-    'script-3': os.path.join(template_dir, 'script-template-3.py')
+    'script-3': os.path.join(template_dir, 'script-template-3.py'),
+    'readme': os.path.join(template_dir, 'README.md')
 }
 
 
