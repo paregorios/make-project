018372d3ee586ce4d5a39ec2f58e7fb825749503
Tom Elliott <tom.elliott@nyu.edu>
2016-08-10 10:35:47 -0500

    data about licenses

---
+++ licenses.py
@@ -0,0 +1,154 @@
+LICENSES = {
+    'afl-3.0': {
+        'title': 'Academic Free License v3.0',
+        'abbr': 'AFL',
+        'classifier': 'License :: OSI Approved :: Academic Free License (AFL)',
+        'src': '::cal'
+    },
+    'agpl-3.0': {
+        'title': 'GNU Affero General Public License v3.0',
+        'classifier': 'License :: OSI Approved :: GNU Affero General Public '
+                      'License v3',
+        'src': '::cal'
+    },
+    'apache-2.0': {
+        'title': 'Apache License 2.0',
+        'classifier': 'License :: OSI Approved :: Apache Software License',
+        'src': '::cal'
+    },
+    'artistic-2.0': {
+        'title': 'Artistic License 2.0',
+        'classifier': 'License :: OSI Approved :: Artistic License',
+        'src': '::cal'
+    },
+    'bsd-2-clause': {
+        'title': 'BSD 2-clause "Simplified" License',
+        'classifier': 'License :: OSI Approved :: BSD License',
+        'src': '::cal'
+    },
+    'bsd-2-clause': {
+        'title': 'BSD 2-clause "Simplified" License',
+        'classifier': 'License :: OSI Approved :: BSD License',
+        'src': '::cal'
+    },
+    'bsd-3-clause-clear': {
+        'title': 'BSD 3-clause Clear License',
+        'classifier': 'License :: OSI Approved :: BSD License',
+        'src': '::cal'
+    },
+    'bsd-3-clause': {
+        'title': 'BSD 3-clause "New" or "Revised" License',
+        'classifier': 'License :: OSI Approved :: BSD License',
+        'src': '::cal'
+    },
+    'eupl-1.1': {
+        'title': 'European Union Public License 1.1',
+        'classifier': 'License :: OSI Approved :: European Union Public '
+                      'Licence 1.1 (EUPL 1.1)',
+        'src': '::cal'
+    },
+    'cc0-1.0': {
+        'title': 'CC0 1.0 Universal Public Domain Dedication',
+        'classifier': 'License :: CC0 1.0 Universal (CC0 1.0) Public Domain '
+                      'Dedication',
+        'src': '::cal'
+    },
+    'gfdl-1.3': {
+        'title': 'GNU Free Documentation License 1.3',
+        'classifier': 'License :: OSI Approved :: GNU Free Documentation '
+                      'License (FDL)',
+        'src': 'https://www.gnu.org/licenses/fdl.txt'
+    },
+    'gpl-2.0': {
+        'title': 'GNU General Public License, version 2',
+        'classifier': 'License :: OSI Approved :: GNU General Public License '
+                      'v2 (GPLv2)',
+        'src': 'https://www.gnu.org/licenses/gpl-2.0.txt'
+    },
+    'gpl-3.0': {
+        'title': 'GNU General Public License, version 3',
+        'classifier': 'License :: OSI Approved :: GNU General Public License '
+                      'v3 (GPLv3)',
+        'src': 'https://www.gnu.org/licenses/gpl-3.0.txt'
+    },
+    'lgpl-2.1': {
+        'title': 'GNU Lesser General Public License v2.1',
+        'classifier': 'License :: OSI Approved :: GNU Lesser General Public '
+                      'License v2 (LGPLv2)',
+        'src': 'https://www.gnu.org/licenses/lgpl-2.1.txt'
+    },
+    'lgpl-3.0': {
+        'title': 'GNU Lesser General Public License v3.0',
+        'classifier': 'License :: OSI Approved :: GNU Lesser General Public '
+                      'License v3 (LGPLv3)',
+        'src': 'https://www.gnu.org/licenses/lgpl-3.0.txt'
+    },
+    'isc': {
+        'title': 'ISC License',
+        'classifier': 'License :: OSI Approved :: ISC License (ISCL)',
+        'src': '::cal'
+    },
+    'mit': {
+        'title': 'MIT License',
+        'classifier': 'License :: OSI Approved :: MIT License',
+        'src': '::cal'
+    },
+    'mpl-2.0': {
+        'title': 'Mozilla Public License 2.0',
+        'classifier': 'License :: OSI Approved :: Mozilla Public License 2.0 '
+                      '(MPL 2.0)',
+        'src': '::cal'
+    },
+    'other': {
+        'title': 'Other/Proprietary License',
+        'classifier': 'License :: Other/Proprietary License'
+    },
+    'cc-by-4.0': {
+        'title': 'Creative Commons Attribution 4.0',
+        'src': '::cal'
+    },
+    'cc-by-sa-4.0': {
+        'title': 'Creative Commons Attribution Share Alike 4.0',
+        'src': '::cal'
+    },
+
+    'epl-1.0': {
+        'title': 'Eclipse Public License 1.0',
+        'src': '::cal'
+    },
+
+    'lppl-1.3c': {
+        'title': 'LaTeX Project Public License v1.3c',
+        'src': '::cal'
+    },
+
+    'ms-pl': {
+        'title': 'Microsoft Public License',
+        'src': '::cal'
+    },
+
+    'ms-rl': {
+        'title': 'Microsoft Reciprocal License',
+        'src': '::cal'
+    },
+
+    'ofl-1.1': {
+        'title': 'SIL Open Font License 1.1',
+        'src': '::cal'
+    },
+
+    'osl-3.0': {
+        'title': 'Open Software License 3.0',
+        'src': '::cal'
+    },
+
+    'unlicense': {
+        'title': 'The Unlicense',
+        'src': '::cal'
+    },
+
+    'wtfpl': {
+        'title': '"Do What The F*ck You Want To Public License"',
+        'src': '::cal'
+    }
+}
